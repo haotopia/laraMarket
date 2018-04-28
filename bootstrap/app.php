97aa0402fe355f726ad4e22d1fc50300ed286fdb
haotopia <haotopia@icloud.com>
2018-04-28 14:38:40 +0800

    Debug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/helpers.php'
+require_once __DIR__ . '/helpers.php';
 /*
 |--------------------------------------------------------------------------
 | Create The Application
