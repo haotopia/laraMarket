94dd9cfb7e3d8b43eb5d4fe0d47963489fbf11da
haotopia <haotopia@icloud.com>
2018-05-01 11:46:50 +0000

    Debug

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -4,36 +4,23 @@ namespace App\Http\Controllers;
 
 use Log;
 
-class WeChatController extends Controller {
+class WechatController extends Controller {
+
 	/**
-	 *处理微信请求
+	 * 处理微信的请求消息
 	 *
-	 *@return string
-	 *
+	 * @return string
 	 */
-	public function server() {
-		Log::info('request arrived.');
-		$app = app('wechat.official_account');
-		$app->server->push(function ($message) {
-			return "欢迎关注 Haotopia！";
+	public function serve() {
+		Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+		$wechat = app('wechat');
+		$wechat->server->setMessageHandler(function ($message) {
+			return "欢迎关注 overtrue！";
 		});
 
 		Log::info('return response.');
 
-		return $app->server->server();
+		return $wechat->server->serve();
 	}
-
-	public function menu_add() {
-		$app = app('wechat');
-		$menu = $app->menu;
-		$button = [
-			[
-				"type" => "view",
-				"name" => "进入商城",
-				"url" => BASE_URL . "",
-			],
-		];
-		$menu->add($button);
-	}
-
 }
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -4,25 +4,38 @@ namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
-class VerifyCsrfToken extends Middleware
-{
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
-    protected $except = [
-        'wechat',
-    ];
-    protected function tokensMatch($request){
-        $token=$request->ajax()?$request->header('X-CSRF-TOKEN'):$request->input('_token');
-        return $request->session()->token() == $token;
-    }
-    public function handle($request,\Closure $next){
-    if($request->method() == 'POST')
-    {
-        return $next($request);
-    }
+class VerifyCsrfToken extends Middleware {
+	/**
+	 * The URIs that should be excluded from CSRF verification.
+	 *
+	 * @var array
+	 */
+	protected $except = [
+		'wechat',
+	];
+	protected function tokensMatch($request) {
+		$token = $request->ajax() ? $request->header('X-CSRF-TOKEN') : $request->input('_token');
+		return $request->session()->token() == $token;
+	}
+	public function handle($request, \Closure $next, $guard = null) {
+		/*if (empty(session("id"))){
+			            $user = session('wechat.oauth_user');
+			            $openid = $user['id'];
+			    　　　　　　//检测数据库中用户账号和微信号是否绑定
+			            $result = WxStudent::check_boundwechat($openid);
+			            if ($result=='200'){
+			                return $next($request);
+			            }else{
+			                return response("请登录", 403)->header("X-CSRF-TOKEN", csrf_token());
+			            }
+			        } else if(!empty(session("id"))) {
+			            return $next($request);
 
-    return parent::handle($request,$next);
+		*/
+		if ($request->method() == 'POST') {
+			return $next($request);
+		}
+
+		return parent::handle($request, $next);
+	}
 }
