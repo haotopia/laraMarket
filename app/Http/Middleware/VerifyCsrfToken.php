7f01c2f34d5b8e4babcc4bdfa3b22a2f4841e3bf
haotopia <haotopia@icloud.com>
2018-05-01 11:40:17 +0000

    Local conf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -18,19 +18,11 @@ class VerifyCsrfToken extends Middleware
         $token=$request->ajax()?$request->header('X-CSRF-TOKEN'):$request->input('_token');
         return $request->session()->token() == $token;
     }
-    public function handle($request,\Closure $next,$guard = null){
-if (empty(session("id"))){
-        $user = session('wechat.oauth_user');
-        $openid = $user['id'];
-　　　　　　//检测数据库中用户账号和微信号是否绑定
-        $result = WxStudent::check_boundwechat($openid);
-        if ($result=='200'){
-            return $next($request);
-        }else{
-            return response("请登录", 403)->header("X-CSRF-TOKEN", csrf_token());
-        }
-    } else if(!empty(session("id"))) {
+    public function handle($request,\Closure $next){
+    if($request->method() == 'POST')
+    {
         return $next($request);
     }
-    }
+
+    return parent::handle($request,$next);
 }
